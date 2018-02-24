c4699176130d52fb8c9f2be064833443a2a6b11c
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-02-24 23:07:02 +0000

    Setup webpack config and add deps.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jptv-keyboard.bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+}
