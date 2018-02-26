3cedc7ff0fd3bd63c436bc4ed265b69ee74f5654
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-02-26 03:21:36 +0000

    Unicode music characters and note display class names.

--- src/components/noteDisplay/noteDisplay.js
+++ src/components/noteDisplay/noteDisplay.js
@@ -1,6 +1,8 @@
+import './note-display.css';
+
 export default React => props => {
   return (
-    <div>
+    <div className="jptv-keyboard-note-display">
       <h4>{ props.note }</h4>
     </div>
   );
