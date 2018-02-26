c39b0762d314305997f32439737feafd127e8172
amyvpellegrini@gmail.com <amyvpellegrini@gmail.com>
2018-02-26 02:11:26 +0000

    Create slide layout and styles. Add slide box.

--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -17,15 +17,31 @@ export default React => () => {
   });
 
   return (
-    <div className="jptv-keyboard-wrapper">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        width="1197"
-        height="120">
-        { keys }
-      </svg>
+    <div className="jptv-keyboard">
+      <div className="jptv-keyboard-wrapper">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          width="2080"
+          height="auto"
+          viewBox="0 0 1197 120">
+          { keys }
+        </svg>
+      </div>
+      <div className="jptv-small-keyboard">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          width="800"
+          height="auto"
+          viewBox="0 0 1197 120">
+          { keys }
+        </svg>
+        <div className="slide-box">
+        </div>
+      </div>
     </div>
   );
 }
